1739da0a8c14942c239670ca97534a6bc709046b
Claudio Geronimo <claudiogeronimo@gmail.com>
2021-11-23 19:56:44 -0300

    Upda MVC and API to Use MediatR

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.IoC;
+using MediatR;
 
 namespace CleanArch.Mvc
 {
@@ -50,6 +51,8 @@ namespace CleanArch.Mvc
                 options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMediatR(typeof(Startup));
 
             RegisterServices(services);
         }
